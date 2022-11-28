469c414f301269729061a5889068e9f67759548d
FifiBro <106842493+FifiBroa@users.noreply.github.com>
2022-11-29 02:04:09 +0300

    LastVersion

---
+++ LessonLast/RhoumbusControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LessonLast
+{
+    /// <summary>
+    /// Логика взаимодействия для RhoumbusControl.xaml
+    /// </summary>
+    public partial class RhoumbusControl : UserControl
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public ImageSource ColoredSourse { get; set; }
+        public ImageSource BlackWhiteSourse =>
+            (ColoredSourse == null) ? null : new FormatConvertedBitmap((BitmapSource)ColoredSourse , PixelFormats.Gray8 , null , 0);
+
+        public RhoumbusControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void BtnAction_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Description, Header, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
